ad096def48050404fd4dff27fd3ade89ea750a95
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2022-09-11 22:37:25 +0300

    11.9.2022

---
+++ src/test/java/HTML.java
@@ -0,0 +1,104 @@
+public class HTML {
+
+                  // Hyper Text Markup Language(HTML)
+
+    /*
+    HTML, Web sayfaları hazırlamak için kullanılan bir İŞARETLEME DİLİDİR.
+    * Html komutlarına "tag" denir.
+    * • Taglar  "< >" işaretlerinin arasına yazılır.
+    Örneğin; : <html> <br> <u> <p> vb gibi
+    * İstisnalar dışında tagların başlangıç ve bitişleri vardır. Bu iki tagların arasına kullanırız
+    Bitiş tagları "«/»" işareti ile belirtilir.
+    Örneğin; <p> Bugün HTML taglarını öğreniyoruz</p> vb gibi
+
+   * VS Code da yeni proje açtığımızda; yeni bir klasör(new folder) oluşturup İçine img, css
+    klasörlerini(new folder) ve index.html dosyasını(new file) oluştururuz. Bu klasörleri olusturmak
+    zorunde değildir. Ama genel kabul görmüş düzeni böyledir. Hem code clear lığı açısında öemli
+    bir aşamadır.
+
+    * HTML 5, büyük / küçük harfe duyarlı değildir.(ignore case sensetive) Ama genellikle küçük harflerle yazılırlar.
+     */
+
+                          // En Temel HTML Tagları
+
+    /*
+    * En dışta  <html> </html>tag’leri yer alıyor.
+    * Sonra gelen tag <head> </head>
+    * daha sonraki tag <body> </body>’dir.
+    * Head tag’i içerisinde yer alan <title> </title> tag’i web sayfamızın başlık bilgisini içermektedir.
+    * Buraya yazacağınız başlık tarayıcının en üstünde görünen başlık olacaktır.
+    * head tag ı kapandıktan sonra açılan tag <body> </body> tagı dir.
+    * içerik ve görünümle ilgili olanları ise body tag’i içerisine yazıyoruz.
+
+    * <!–…–> Yorum tagıdır, içine yorumlarımızı yazarız web sayfasında görünmez.
+
+    *<p> (paragraph) tagı => Yazıları paragraf haline getirir.
+
+    * <br> (break) tagı => Metin içinde satır başı yapmak için kullanılır.
+
+    * <h1> to <h6> (head) tagı => HTML başlıkları tanımlar, Standart 6 tane başlık tagı bulunmaktadır.
+     <h1> en büyük başlık, <h6> ya kadar başlık puntosu küçülerek gider.
+
+    * <hr> (horizontal row) tagı => Yatay çizgi oluşturmak için kullanılır.
+
+    * <strong> tagı => Metin yazılarını kalınlaştırmak için kullanılır.
+
+    * <i> (italic) tagı => Yazıyı eğik (italik) yazdırmak için kullanılır.
+
+    * <u> (underline) tagı => Yazının altını çizmek için kullanılır.
+
+    ** İç içe tag kullanıldığımızda Önce açılan tag en son kapatılır. Buna dikkat etmek gerekiyor.
+     Yoksa çıktımızda istediğimiz sonucu alamayabiliriz. Yani FILO(First In Last Out)
+
+    * <a> (anchor) tagı => Bir sayfadan başka bir sayfaya ya da farkı bir siteye bağlantı vermek için kullanılır.
+
+    * Kullanıcıyla iletişime geömek için kullanılan yapılara form denir. Form içinde textbox, button,
+   radio button, checkbox vb elemanlar olabilir.
+
+   * <form>  => Kullanıcı girişi için bir HTML formu tanımlar. Tüm form elemanlarını içinde barındırır.
+    form yapısı <from> ile başlayıp </form> ile biter.
+
+   * * <input>  => Kullanıcıdan bilgi almak için kullanılır.
+
+   * * <label> =>  Kullanıcıya form elemanı ile ilgili bilgi vermek için kullanılır.
+
+   * <input> elemanı içinde;
+    checkbox => Kullanıcıya birden fazla işaretleme yapılabilecek seçenekler sunar.
+    radio => Kullanıcıya sadece tek seçim yapabileceği seçenekler sunar. Ya onu seçeceksin ya bunu gibi.
+
+   * * <select> tagı => Kullanıcıya listeden seçim yaptırmak için kullanılır. Her bir seçenek ise <option> tagı
+    içine konur.
+
+    * button => Genel amaç için button oluşturur. Butonun default olarak bir görevi yoktur.
+    * submit => Buton tuşunu aktifleştirir.
+    * reset => Formdaki bilgileri resetleyen bir buton oluşturur.
+
+    * <div> ve <span> tagları => Biçimlendirme olarak hiçbir görsel katkısı olmayan sadece html elemanlarını
+     gruplamak için kullanılan taglardır.
+     <div> tagı => Blok elemandır, kapladığı alan ne olursa olsun tüm satırı işgal eder.
+     <span> => Inline elemandır, sadece kapladığı alan kadar yer işgal eder.
+
+
+
+     */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
